49bba1b0639c7e7e5a4ed25b49edca89a1305936
killyan <piriou.killyan@gmail.com>
2023-03-16 15:25:31 +0100

    pseudos

--- script.js
+++ script.js
@@ -9,8 +9,14 @@ const de5 = document.getElementById("des5");
 const de6 = document.getElementById("des6");
 const scorePlayer1 = document.getElementById("scorePlayer1");
 const scorePlayer2 = document.getElementById("scorePlayer2");
-const Player = document.getElementById("player");
+const player = document.getElementById("player");
 const point = document.getElementById("point");
+const user1 = document.getElementById("pseudo1");
+const user2 = document.getElementById("pseudo2");
+const btnComfirm = document.getElementById("comfirm");
+const divPseudo = document.getElementById("pseudos");
+const table_scores = document.getElementById("table_scores");
+const flex_button = document.getElementById("flex_button");
 
 let countPlayer1 = 0;
 let countPlayer2 = 0;
@@ -18,26 +24,43 @@ let countPoint = 0;
 let currentPlayer = "player1";
 const countWin = 100;
 let gameWin = false;
+let pseudo1 = null;
+let pseudo2 = null;
 
 function random(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-   return Math.floor(Math.random()* (max - min)) + min;
+   return Math.floor(Math.random()* (max - min + 1)) + min;
 }
+
+btnComfirm.addEventListener('click', e=>{
+    if (user1.value === "" || user2.value === "") {
+        alert("Au moins l'un des champs n'a pas était remplie !")
+    }else{
+        pseudo1 = user1.value;
+        pseudo2 = user2.value;
+        divPseudo.style.display = 'none';
+        flex_button.style.display ="flex"
+        table_scores.style.display = 'block';
+        player.style.display ="block";
+        player.innerHTML = pseudo1;
+        point.style.display="block";
+    }
+})
 
 btnPoint.addEventListener('click', e =>{
     if (countPlayer1 < countWin && countPlayer2 < countWin && countPoint > 0){
         if(currentPlayer === "player1"){
             countPlayer1 += countPoint;
             scorePlayer1.innerHTML = countPlayer1;
-            Player.innerHTML = "Joueur 2";
+            player.innerHTML = pseudo2;
             countPoint = 0;
             currentPlayer = "player2";
         }
         else{
             countPlayer2 += countPoint;
             scorePlayer2.innerHTML=countPlayer2;
-            Player.innerHTML= "Joueur 1";
+            player.innerHTML= pseudo1;
             countPoint = 0;
             currentPlayer = "player1";
         };
@@ -47,14 +70,14 @@ btnPoint.addEventListener('click', e =>{
 
     if(countPlayer1 >= countWin){
         countPlayer1 = countWin
-        alert("player1 win!")
+        alert(pseudo1 +" a gagner!")
         btnPoint.style.display='none';
         btnReplay.style.display='block';
         gameWin=true;
     }
     else if(countPlayer2 >= countWin){
         countPlayer2 = countWin
-        alert("player2 win!")
+        alert(pseudo2 +" a gagner!")
         btnPoint.style.display='none';
         btnReplay.style.display='block';
         gameWin=true;
@@ -70,11 +93,11 @@ btn.addEventListener('click', e=>{
             drawDe(1)
             if(currentPlayer === "player1"){
                 currentPlayer = "player2"
-                Player.innerHTML = "Joueur 2"
+                player.innerHTML = pseudo2
             }
             else{
                 currentPlayer = "player1"
-                Player.innerHTML = "Joueur 1"
+                player.innerHTML = pseudo1
             }
         }else{
             drawDe(rand)
@@ -94,6 +117,12 @@ btnReplay.addEventListener('click', e=>{
     scorePlayer1.innerHTML=0;
     currentPlayer = "player1";
     gameWin=false;
+
+    divPseudo.style.display = 'flex';
+    flex_button.style.display ="none"
+    table_scores.style.display = 'none';
+    player.style.display = "none";
+    point.style.display="none";
 })
 
 function drawDe(pcurrentDe){
